cb7ba3d9a33963312892ea175bf9ea0ec15d819f
Tino <Tino190991@live.dk>
2018-07-12 17:59:01 +0200

    ngn

--- assets/script/portfolio.js
+++ assets/script/portfolio.js
@@ -135,4 +135,35 @@ function showArticles(n) {
   }
   // assigner style "display block" to 1 slide at (js values start at 0)
   slides[slideIndex-1].style.display = "block";
+  showSubSlides(n)
+}
+
+
+// -----------------------------------------------------------------------------
+
+
+function showSubSlides(n) {
+  var i;
+  // locating and assigning DIVs, with the class "mySlides" assigned, to array "slides"
+  var slides = document.getElementsByClassName("articleSubSlides");
+  // if imported js value is higher than the amount of imported "slides" (.mySlides) assign 1 to js value "slideIndex"
+  if (n > slides.length) {
+    slideIndex = 1}
+  // if imported js value is less than 1 assign slideIndex the value equal to the amount of "slides" (.mySlides) imported
+  if (n < 1) {
+    slideIndex = slides.length}
+  // running loop times equal to the amount of "slides"
+  for (i = 0; i < slides.length; i++) {
+    // assigner style "display none" to slides(1), slides(2), and slide(3) if only 3 slides are used on the site
+    slides[i].style.display = "none";
+  }
+  // assigner style "display block" to 1 slide at (js values start at 0)
+  slides[slideIndex-1].style.display = "block";
+}
+
+// function run when pressing pointers (value either 1 0r -1)
+function plusSubSlides(n) {
+  console.log("running plusSubSlides");
+  // "adding" n value to that of slideIndex
+  showSubSlides(slideIndex += n);
 }
